a5513a71aaebd28f86fe6cedc2ca1d8b8b33ea9a
CSU-Ryan <enderbro@colostate.edu>
2024-03-11 21:39:37 -0600

    Lots of things in base 2.

--- base2.py
+++ base2.py
@@ -1,38 +1,33 @@
-import numpy as np
+from timeit import default_timer as timer
 
 primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157]
 
 
-def fast_exponentiation(base, exponent):
-    result = 1
-    while exponent > 0:
-        if exponent % 2 == 1:
-            result *= base
-        base *= base
-        exponent //= 2
-    return result
-
-
 def binary_to_prime(number: int):
-    pos = 0
-    total = 1
+    product = 1
+    prime_iter = iter(primes)
+
     while number > 0:
-        bit = number & 1
+        next_prime = next(prime_iter)
+        if (number & 1) == 1:
+            product *= next_prime
+        number >>= 1
 
-        if bit == 1:
-            total *= primes[pos]
-        number = number >> 1
-        pos += 1
-
-    return total
+    return product
 
 
-start = 100000000
-for number in range(start, start*10):
-# for number in range(1, 1000000):
+# for number in range(start, start*10):
+out = []
+start = timer()
+first_val = 10000000
+for number in range(first_val, first_val*10):
 
-    converted_number = binary_to_prime(number)
+    prime = binary_to_prime(number)
+    # print(f"{number:08b}: {prime}")
+    if number == prime:
+        out.append(number)
 
-    # print(number, converted_number)
-    if number is not None and number == converted_number:
-        print(number)
+end = timer()
+print(f"t: {end - start:0.2f} seconds")
+for number in out:
+    print(f"{number:b}")
