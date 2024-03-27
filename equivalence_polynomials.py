5775d02ac5cf085346865f4d1515d27674a97ae7
EnderBro1000 <rockers4403@gmail.com>
2024-03-27 15:56:42 -0600

    Replaces polynomial solver

--- equivalence_polynomials.py
+++ equivalence_polynomials.py
@@ -16,7 +16,7 @@ def fast_exp(base: float, exponent: float) -> float:
             exponent -= 1
 
         base *= base
-        exponent /= 2
+        exponent //= 2
 
     return result
 
@@ -48,21 +48,67 @@ def get_root(roots):
             return np.real(x)
 
 
-def find_equivalence_polynomials(digits, val_prime):
+def solve_linear(coefficients):
+    a = coefficients[0]
+    b = coefficients[1]
+
+    if b == 0:
+        return None
+
+    x = (-a) / b
+
+    # Check for integer solution
+    if x % 1 != 0:
+        return None
+
+    return x
+
+
+def solve_quadratic(coefficients):
+    c = coefficients[0]
+    b = coefficients[1]
+    a = coefficients[2]
+
+    # Check if the system is actually quadratic
+    if a == 0:
+        return solve_linear(coefficients[:2])
+
+    in_sqrt = b*b - (4 * a * c)
+
+    if in_sqrt < 0:
+        return None
+    else:
+        x = (-b + np.sqrt(in_sqrt)) / (2 * a)
+
+    # Check for integer solution
+    if x % 1 != 0 or x < 0:
+        return None
+
+    return x
+
+
+def find_equivalence_polynomials(digits: list, val_prime):
     digits[0] -= val_prime
+
+    if len(digits) == 2:
+        return solve_linear(digits)
+
+    if len(digits) == 3:
+        return solve_quadratic(digits)
+
+    # Last resort, using numpy solver.
     polynomial = npoly.Polynomial(digits)
-    # polynomial.domain = np.array([2, 10])
 
     roots = polynomial.roots()
     return get_root(roots)
 
 
 if __name__ == '__main__':
-    base = 1000
+    base = 10
     digits = np.array([0, 1, 0], dtype=float)
-    end = 1000
+    end = 100
 
-    bases = np.zeros((end-1), dtype=float)
+    bases = np.zeros((end-1), dtype=float, )
 
     for i in range(end-1):
         val_prime = base_prime(digits)
@@ -70,12 +116,12 @@ if __name__ == '__main__':
         root = find_equivalence_polynomials(np.copy(digits), val_prime)
 
         if root is not None:
-            # print(f"repr:{digits} b:{root} -> val:{val_prime}")
+            print(f"repr:{digits} b:{root} -> val:{val_prime}")
             bases[i] = root
 
         digits = increment_digits(digits, base)
 
-    print(bases)
-    plt.scatter(list(range(len(bases))), bases)
-    plt.yscale("log")
-    plt.show()
+    # print(bases)
+    # plt.scatter(list(range(len(bases))), bases)
+    # plt.yscale("log")
+    # plt.show()
