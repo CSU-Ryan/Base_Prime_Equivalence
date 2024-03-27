60c68e3c349f0283600e8bb0fb523d4cef274cc1
CSU-Ryan <enderbro@colostate.edu>
2024-03-27 15:09:04 -0600

    Changes primary data type to float and removes unused functions.

--- equivalence_polynomials.py
+++ equivalence_polynomials.py
@@ -1,51 +1,37 @@
 import numpy as np
 import numpy.polynomial as npoly
 import sympy
+import matplotlib.pyplot as plt
 
 
-def fast_exp(base: int, exponent: int) -> int:
+def fast_exp(base: float, exponent: float) -> float:
     """
     A fast algorithm for exponentiation
     """
     result = 1
     while exponent > 0:
+
         if exponent % 2 == 1:
             result *= base
+            exponent -= 1
+
         base *= base
-        exponent //= 2
+        exponent /= 2
+
     return result
 
 
-def string_to_digits(string: str) -> list:
-    out = []
-    for digit in string[::-1]:
-        out.append(int(digit))
-    return out
-
-
-def base_to_digits(n, base) -> list:
-    digits = []
-
-    while n > 0:
-        digits.append(n % base)
-        n //= base
-
-    return digits
-
-
-def base_prime(digits: list) -> int:
+def base_prime(digits) -> int:
     total = 1
     primes = [sympy.prime(n+1) for n in range(len(digits))]
-    # prime = 2
 
     for digit, prime in zip(digits, primes):
         total *= fast_exp(prime, digit)
-        # prime = sympy.nextprime(prime)
 
     return total
 
 
-def increment_digits(digits: list, base: int, pos=0) -> list:
+def increment_digits(digits, base: int, pos=0):
     while digits[pos] + 1 >= base:
         digits[pos] = 0
         pos += 1
@@ -56,13 +42,13 @@ def increment_digits(digits: list, base: int, pos=0) -> list:
 
 
 def get_root(roots):
-    epsilon = 0.1
+    epsilon = 0.05
     for x in roots:
-        if np.imag(x) < epsilon and np.real(x) % 1 < epsilon:
-            return x
+        if np.imag(x) < epsilon and (np.real(x) % 1) < epsilon:
+            return np.real(x)
 
 
-def find_equivalence_polynomials(digits: list, val_prime):
+def find_equivalence_polynomials(digits, val_prime):
     digits[0] -= val_prime
     polynomial = npoly.Polynomial(digits)
     # polynomial.domain = np.array([2, 10])
@@ -72,16 +58,24 @@ def find_equivalence_polynomials(digits: list, val_prime):
 
 
 if __name__ == '__main__':
-    base = 10
-    digits = [0, 0, 0]
-    end = 100
+    base = 1000
+    digits = np.array([0, 1, 0], dtype=float)
+    end = 1000
 
-    for _ in range(end-1):
+    bases = np.zeros((end-1), dtype=float)
+
+    for i in range(end-1):
         val_prime = base_prime(digits)
 
-        root = find_equivalence_polynomials(digits[:], val_prime)
+        root = find_equivalence_polynomials(np.copy(digits), val_prime)
 
         if root is not None:
-            print(f"repr:{str(digits[::-1]):12s} b:{int(root)} -> val:{val_prime}")
+            # print(f"repr:{digits} b:{root} -> val:{val_prime}")
+            bases[i] = root
 
         digits = increment_digits(digits, base)
+
+    print(bases)
+    plt.scatter(list(range(len(bases))), bases)
+    plt.yscale("log")
+    plt.show()
